c40be9293c2737e378d3a33c35f3e22d22e3128a
Dewolfus <spacetvc@gmail.com>
2025-01-27 11:56:55 +0200

    update

--- STM/gamelogic/Core/Src/main.c
+++ STM/gamelogic/Core/Src/main.c
@@ -22,6 +22,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include "math.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -59,7 +60,13 @@ static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
-
+int Get_float(float number);
+int calculateBCC(uint8_t *data, int length, bool get);
+void Check_Protocol();
+void game_control(void);
+void reset_game(void);
+void update_game(void);
+void send_game_state(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -83,225 +90,19 @@ float ball_dy = 0.5;
 float platform_x = 6;
 float platform_y = SCREEN_HEIGHT - 4;
 bool game_over = 0;
-bool paused = 1;  // Paused default status
+bool paused = 0;  // Paused default status
 int game_type = 0;  // 0 - Pong, 1 - Snake
 bool connect_req = 0;
 bool game_req=0;
 
+bool *ptrPaused = &paused;
 uint8_t msg_rx[16];
 uint8_t msg_tx[128];
 
-void reset_game() {
-    ball_x = 8;
-    ball_y = 8;
-    platform_x = 6;
-    platform_y = SCREEN_HEIGHT - 4;
-    game_over = 0;
-}
-int calculateBCC(uint8_t *data, int length, bool get) // BCC CALCULATION 1-true 0-false
-{	
-	
-   int checksum = 0;
-	if(!get){
-			
-    for (int i = 0; i < length-1; i++)
-    {
-        checksum ^= (int)data[i];
-	
-
-				
-    }
-	
-		if(	checksum == data[length-1]){
-    return 1;
-		}
-		else{
-			return 0;
-		}
-	}
-	else if(get) {
-
-		  for (int i = 0; i < length-1; i++)
-    {
-        checksum ^= (int)data[i];
-			
-    }
-
-	
-
-	
-		
-	}
-		return checksum;
-}
-
-void game_control(){
-		if (msg_rx[1] == 'a' && !paused && platform_x > 1) {
-            platform_x -= 1;
-			}
-		else if ((char)msg_rx[1] == 'd' && !paused && platform_x < SCREEN_WIDTH - PLATFORM_WIDTH - 1) {
-            platform_x += 1;
-      }
-		else if (((char)msg_rx[1] == 'r') && !paused ){
-					reset_game();
-			}
-		else if ((char)msg_rx[1] == 'p'){
-					paused = !paused; // Toggle pause state
-			}
-		else if((int)msg_rx[1] == 33){ // ASCII 033 = ESC
-				 msg_tx[0] = 0x01;
-				 msg_tx[1] = 0x01;
-				 msg_tx[2] = calculateBCC(msg_tx, 3, 1);
-				 HAL_UART_Transmit(&huart1, msg_tx, 3 , 0xFFFF);	 
-			}
-				 
-					 
-					msg_tx[0] = 0x03;
-					msg_tx[1] = ball_x;
-					msg_tx[2] = ball_y;
-					msg_tx[3] = platform_x;
-					msg_tx[4] = platform_y;
-					msg_tx[5] = calculateBCC(msg_tx, 8, 1);
-
-        HAL_UART_Receive_IT(&huart1, msg_rx, 1);
-					 
-}
-
-
-
-
-
-void Check_Protocol(){ // Protocol --------------------------------------------------------------------------------
-
-	switch((int)msg_rx[0]){
-		 
-		 
-		 
-		case(0x01): // MENU SELECT
-
-				if(calculateBCC(msg_rx, 3, 0 ) && !(int)msg_rx[1]){
-					// NEED TO MAKE CYCLE FOR LIST OF GAMES (BLALALA BLYLYLYL)
-					  connect_req =1; // CONNECT ALLOW
-						msg_tx[0]=0x01;
-						msg_tx[1]=0x01;
-						msg_tx[2]=0x14;
-						msg_tx[3]=calculateBCC(msg_tx, 4,1);
-			      HAL_UART_Transmit_IT(&huart1, msg_tx, 4);}
-				else{
-						msg_tx[0]=0x01;
-						msg_tx[1]=0xFF;
-						msg_tx[2]=0xFF;
-						msg_tx[3]=calculateBCC(msg_tx, 4,1);		
-						HAL_UART_Transmit(&huart1, msg_tx, 4 , 0xFFFF);}
-			break;
-		
-		case(0x02): // GAME SELECT
-
-		    if(calculateBCC(msg_rx, 5, 0 )){ // CHECK CORRECT BCC	
-					
-					
-					if( ((int)msg_rx[1] == 0x0E) &&( !(int)msg_rx[2] && !(int)msg_rx[3]))  {// PC choosing game (XX-number of game)--------------------------
-						msg_tx[0]=0x02;
-						msg_tx[1]=0x0E;
-						msg_tx[2]=SCREEN_WIDTH;
-						msg_tx[3]=SCREEN_HEIGHT;
-						msg_tx[4] =calculateBCC(msg_tx, 5,1);		
-						HAL_UART_Transmit(&huart1,msg_tx,5,0xFFFF); // NEED REMAKE STM > PC     X_S/Y_S - size of map
-						paused = 0;
-									 
-					}
-					else if((int)msg_rx[1] != 0x0E){ // NOT CORRECT GAME 
-						msg_tx[0]=0x02;
-						msg_tx[1]=0xDD;
-						msg_tx[2]=0xFF;
-						msg_tx[3]=calculateBCC(msg_tx, 4,1);
-			      HAL_UART_Transmit(&huart1, msg_tx, 4 , 0xFFFF);
-					
-					}
-			
-					}
-				else {    // BCC ERROR
-					
-						msg_tx[0]=0x02;
-						msg_tx[1]=0xFF;
-						msg_tx[2]=0xFF;
-						msg_tx[3]=calculateBCC(msg_tx, 4,1);
-			      HAL_UART_Transmit(&huart1, msg_tx, 4 , 0xFFFF);
-										
-				}
-				
-			break;	
-		
-		
-		
-
-		case(0x04):
-			game_control();
-			//recive control key
-			break;
-			
-			
-		
-
-		
-
-	
-		break;
- }
-}
-
-
-
-
-
-
-
-void update_game() {
-    if (!game_over) {
-        ball_x += ball_dx;
-        ball_y += ball_dy;
-
-        if (ball_x <= 1 || ball_x >= SCREEN_WIDTH - 2) {
-            ball_dx = -ball_dx;
-        }
-        if (ball_y <= 1) {
-            ball_dy = -ball_dy;
-        }
-
-        if (ball_y == (platform_y) && ball_x >= platform_x && ball_x < platform_x + PLATFORM_WIDTH) {
-            ball_dy = -ball_dy;
-        }
-
-        if (ball_y > (platform_y)) {
-            game_over = 1;
-        }
-    } else {
-        reset_game();
-    }
-}
-
-
-
-void send_game_state() { // SEND COORDINATES -------------------------------------
-    				msg_tx[0]=0x03;
-						msg_tx[1]=ball_x;
-						msg_tx[2]=ball_y;
-	          msg_tx[3]=platform_x;
-	          msg_tx[4]=platform_y;
-	          msg_tx[5]=calculateBCC(msg_tx, 6,1);
-						//HAL_UART_Transmit(&huart1, msg_tx, sprintf((char *)msg_tx, " code %x, bx %f, by %f , px %f, py %f  -----", 0x03, ball_x, ball_y, platform_x, platform_y), 0xFFFF);
-		       
-	        
-}
-
-
-
 
 void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size){ // PROBABLY NEED CALLBACK REWORK :3
-	  Check_Protocol();
-	HAL_UARTEx_ReceiveToIdle_IT(&huart1, msg_rx, 16);
+  HAL_UARTEx_ReceiveToIdle_IT(&huart1, msg_rx, 16);
 }
-
 
 
 
@@ -309,22 +110,21 @@ int main(void) {
 	  HAL_Init();
     SystemClock_Config();
     MX_USART1_UART_Init();
-	
 
 		HAL_UARTEx_ReceiveToIdle_IT(&huart1, msg_rx, 16);
     while (1) {
-				
-		if(connect_req){ // CONNECT ALLOW
-        	if (!paused) {
-            update_game();
-           send_game_state();
+			Check_Protocol();
+          if (paused) {
+          send_game_state();
+					update_game();
 					HAL_Delay(50);
         }
-	
-			}
+
 }
 
 }
+
+
 /**
   * @brief System Clock Configuration
   * @retval None
@@ -437,6 +237,208 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+int Get_float(float number){
+    return ((int)number==number) ?  (( (int)number << 1 ) | 0) : (( (int)number << 1 ) | 1) ;
+};
+
+int calculateBCC(uint8_t *data, int length, bool get) {// BCC CALCULATION 1-true 0-false	
+	
+   int checksum = 0;
+	if(!get){
+			
+    for (int i = 0; i < length-1; i++)
+    {
+        checksum ^= (int)data[i];
+	
+
+				
+    }
+	
+		if(	checksum == data[length-1]){
+    return 1;
+		}
+		else{
+			return 0;
+		}
+	}
+	else if(get) {
+
+		  for (int i = 0; i < length-1; i++)
+    {
+        checksum ^= (int)data[i];
+			
+    }
+
+	
+
+	
+		
+	}
+		return checksum;
+}
+void game_control(){ 
+		if (msg_rx[1] == 'a' && !paused && platform_x > 1) {
+            platform_x -= 1;
+			}
+		else if ((char)msg_rx[1] == 'd' && !paused && platform_x < SCREEN_WIDTH - PLATFORM_WIDTH - 1) {
+            platform_x += 1;
+      }
+		else if (((char)msg_rx[1] == 'r') && !paused ){
+					reset_game();
+			}
+		else if ((char)msg_rx[1] == 'p'){
+					paused = !paused; // Toggle pause state
+			}
+		else if((int)msg_rx[1] == 33){ // ASCII 033 = ESC
+				 msg_tx[0] = 0x01;
+				 msg_tx[1] = 0x01;
+				 msg_tx[2] = calculateBCC(msg_tx, 3, 1);
+				 HAL_UART_Transmit(&huart1, msg_tx, 3 , 0xFFFF);
+		     game_req=0;
+			}
+				 
+					 
+					msg_tx[0] = 0x03;
+					msg_tx[1] = ball_x;
+					msg_tx[2] = ball_y;
+					msg_tx[3] = platform_x;
+					msg_tx[4] = platform_y;
+					msg_tx[5] = calculateBCC(msg_tx, 8, 1);
+
+        HAL_UART_Receive_IT(&huart1, msg_rx, 1);
+					 
+}
+
+
+
+
+
+void Check_Protocol(){ // Protocol --------------------------------------------------------------------------------
+
+	switch((int)msg_rx[0]){
+		 
+		 
+		 
+		case(0x01): // MENU SELECT
+
+				if(calculateBCC(msg_rx, 3, 0 ) && !(int)msg_rx[1]){
+					// NEED TO MAKE CYCLE FOR LIST OF GAMES (BLALALA BLYLYLYL)
+					  connect_req =1; // CONNECT ALLOW
+						msg_tx[0]=0x01;
+						msg_tx[1]=0x01;
+						msg_tx[2]=0x14;
+						msg_tx[3]=calculateBCC(msg_tx, 4,1);
+			      HAL_UART_Transmit_IT(&huart1, msg_tx, 4);
+			      game_req =0;}
+				else{
+						msg_tx[0]=0x01;
+						msg_tx[1]=0xFF;
+						msg_tx[2]=0xFF;
+						msg_tx[3]=calculateBCC(msg_tx, 4,1);		
+						HAL_UART_Transmit(&huart1, msg_tx, 4 , 0xFFFF);}
+			break;
+		
+		case(0x02): // GAME SELECT
+
+		    if(calculateBCC(msg_rx, 5, 0 )){ // CHECK CORRECT BCC	
+					
+					
+					if( !game_req && ( (int)msg_rx[1] == 0x0E) &&( !(int)msg_rx[2] && !(int)msg_rx[3]))  {// PC choosing game (XX-number of game)--------------------------
+						msg_tx[0]=0x02;
+						msg_tx[1]=0x0E;
+						msg_tx[2]=SCREEN_WIDTH;
+						msg_tx[3]=SCREEN_HEIGHT;
+						msg_tx[4] =calculateBCC(msg_tx, 5,1);	
+						HAL_UART_Transmit(&huart1,msg_tx,5,0xFFFF); // NEED REMAKE STM > PC     X_S/Y_S - size of map
+						paused=1;
+					  game_req =1;
+						
+									 
+					}
+					else if((int)msg_rx[1] != 0x0E){ // NOT CORRECT GAME 
+						msg_tx[0]=0x02;
+						msg_tx[1]=0xDD;
+						msg_tx[2]=0xFF;
+						msg_tx[3]=calculateBCC(msg_tx, 4,1);
+			      HAL_UART_Transmit(&huart1, msg_tx, 4 , 0xFFFF);
+					}
+			
+					}
+				else {    // BCC ERROR
+					
+						msg_tx[0]=0x02;
+						msg_tx[1]=0xFF;
+						msg_tx[2]=0xFF;
+						msg_tx[3]=calculateBCC(msg_tx, 4,1);
+			      HAL_UART_Transmit(&huart1, msg_tx, 4 , 0xFFFF);				
+				}
+				
+			break;	
+		
+		
+		
+
+		case(0x04):
+			game_control();
+			//recive control key
+			break;
+			
+			
+		
+
+		
+
+	
+		break;
+ }
+}
+void reset_game() {
+    ball_x = 8;
+    ball_y = 8;
+    platform_x = 6;
+    platform_y = SCREEN_HEIGHT - 4;
+    game_over = 0;
+}
+
+void update_game() {
+    if (!game_over) {
+        ball_x += ball_dx;
+        ball_y += ball_dy;
+
+        if (ball_x <= 1 || ball_x >= SCREEN_WIDTH - 2) {
+            ball_dx = -ball_dx;
+        }
+        if (ball_y <= 1) {
+            ball_dy = -ball_dy;
+        }
+
+        if (ball_y == (platform_y) && ball_x >= platform_x && ball_x < platform_x + PLATFORM_WIDTH) {
+            ball_dy = -ball_dy;
+        }
+
+        if (ball_y > (platform_y)) {
+            game_over = 1;
+        }
+    } else {
+        reset_game();
+    }
+}
+
+
+
+void send_game_state() { // SEND COORDINATES -------------------------------------
+    				msg_tx[0]=0x03;
+						msg_tx[1]=Get_float(ball_x);
+						msg_tx[2]=Get_float(ball_y);
+	          msg_tx[3]=platform_x;
+	          msg_tx[4]=platform_y;
+	          msg_tx[5]=calculateBCC(msg_tx, 6,1);
+						HAL_UART_Transmit(&huart1, msg_tx, 6, 0xFFFF);
+		       
+	        
+}
+
+
 
 
 
